3d1afa74fb0660b187cc2c5c47ef584a5c9e50de
Abdulsalam Mansour <abdulsalam.mans96@gmail.com>
2022-06-14 17:02:15 +0300

    LD-448 - add DashboardActivity.kt

---
+++ app/src/main/java/com/logestechs/driver/ui/dashboard/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.logestechs.driver.ui.dashboard
+
+import android.os.Bundle
+import com.logestechs.driver.R
+import com.logestechs.driver.utils.LogesTechsActivity
+
+class DashboardActivity : LogesTechsActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
