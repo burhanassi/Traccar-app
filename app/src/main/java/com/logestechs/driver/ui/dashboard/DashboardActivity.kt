f01f1f7b99d45b23edbe04944fe4ada4b2a2131a
Abdulsalam Mansour <abdulsalam.mans96@gmail.com>
2022-06-19 10:12:58 +0300

    LD-448 - wrap up the design for DashboardActivity.kt

--- app/src/main/java/com/logestechs/driver/ui/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/logestechs/driver/ui/dashboard/DashboardActivity.kt
@@ -1,10 +1,12 @@
 package com.logestechs.driver.ui.dashboard
 
 import android.os.Bundle
+import android.view.View
+import com.logestechs.driver.R
 import com.logestechs.driver.databinding.ActivityDashboardBinding
 import com.logestechs.driver.utils.LogesTechsActivity
 
-class DashboardActivity : LogesTechsActivity() {
+class DashboardActivity : LogesTechsActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityDashboardBinding
 
@@ -12,5 +14,20 @@ class DashboardActivity : LogesTechsActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.dashEntryAcceptedPackages.textCount.text = "55"
+        binding.buttonShowDashboardSubEntries.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.button_show_dashboard_sub_entries -> {
+                if (binding.containerDashboardSubEntries.visibility == View.VISIBLE) {
+                    binding.containerDashboardSubEntries.visibility = View.GONE
+                } else {
+                    binding.containerDashboardSubEntries.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
