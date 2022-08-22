64406079f9207b40900abac202ff15a553cd1b8b
Abdulsalam Mansour <abdulsalam.mans96@gmail.com>
2022-08-22 11:51:58 +0300

    LD-473 - add gesture view to PackageDeliveryActivity.kt

--- app/src/main/java/com/logestechs/driver/ui/packageDelivery/PackageDeliveryActivity.kt
+++ app/src/main/java/com/logestechs/driver/ui/packageDelivery/PackageDeliveryActivity.kt
@@ -1,26 +1,113 @@
 package com.logestechs.driver.ui.packageDelivery
 
+import android.gesture.GestureOverlayView
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.os.Environment
+import android.view.MotionEvent
+import android.view.View
+import com.logestechs.driver.R
 import com.logestechs.driver.data.model.Package
 import com.logestechs.driver.databinding.ActivityPackageDeliveryBinding
+import com.logestechs.driver.utils.Helper
 import com.logestechs.driver.utils.IntentExtrasKeys
 import com.logestechs.driver.utils.LogesTechsActivity
+import java.io.File
 
-class PackageDeliveryActivity : LogesTechsActivity() {
+class PackageDeliveryActivity : LogesTechsActivity(), View.OnClickListener {
     private lateinit var binding: ActivityPackageDeliveryBinding
 
+    private var path: String? = null
+    private var file: File? = null
+    private val bitmap: Bitmap? = null
+    var gestureTouch = false
     private var pkg: Package? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPackageDeliveryBinding.inflate(layoutInflater)
         setContentView(binding.root)
         getExtras()
+        initializeUi()
+        initListeners()
+    }
+
+    private fun initializeUi() {
+        path = Environment.getExternalStorageDirectory().toString() + "/signature.png"
+        file = File(path)
+        file?.delete()
+
+//        binding.gestureViewSignature.isDrawingCacheEnabled = true
+//
+//        binding.gestureViewSignature.isAlwaysDrawnWithCacheEnabled = true
+        binding.gestureViewSignature.isHapticFeedbackEnabled = false
+        binding.gestureViewSignature.cancelLongPress()
+        binding.gestureViewSignature.cancelClearAnimation()
+    }
+
+    private fun isSignatureEntered(): Boolean {
+        return binding.gestureViewSignature.gesture != null && binding.gestureViewSignature.gesture.length > 0
+    }
+
+    private fun initListeners() {
+        binding.gestureViewSignature.addOnGestureListener(object :
+            GestureOverlayView.OnGestureListener {
+            override fun onGesture(
+                gestureOverlayView: GestureOverlayView,
+                motionEvent: MotionEvent
+            ) {
+            }
+
+            override fun onGestureCancelled(
+                gestureOverlayView: GestureOverlayView,
+                motionEvent: MotionEvent
+            ) {
+            }
+
+            override fun onGestureEnded(
+                gestureOverlayView: GestureOverlayView,
+                motionEvent: MotionEvent
+            ) {
+                binding.containerScrollView.isEnableScrolling = true
+            }
+
+            override fun onGestureStarted(
+                gestureOverlayView: GestureOverlayView,
+                motionEvent: MotionEvent
+            ) {
+                binding.containerScrollView.isEnableScrolling = false
+                gestureTouch = motionEvent.action != MotionEvent.ACTION_MOVE
+            }
+        })
+
+        binding.buttonClearSignature.setOnClickListener(this)
+        binding.buttonDeliverPackage.setOnClickListener(this)
     }
 
     private fun getExtras() {
         val extras = intent.extras
         if (extras != null) {
             pkg = extras.getParcelable(IntentExtrasKeys.PACKAGE_TO_DELIVER.name)
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.button_clear_signature -> {
+                binding.gestureViewSignature.invalidate()
+                binding.gestureViewSignature.clear(true)
+                binding.gestureViewSignature.clearAnimation()
+                binding.gestureViewSignature.cancelClearAnimation()
+                gestureTouch = false
+            }
+
+            R.id.button_deliver_package -> {
+                if (isSignatureEntered()) {
+                    Helper.showSuccessMessage(this, "done")
+                } else {
+                    Helper.showErrorMessage(this, "signature not enetered")
+                }
+            }
         }
     }
 }
