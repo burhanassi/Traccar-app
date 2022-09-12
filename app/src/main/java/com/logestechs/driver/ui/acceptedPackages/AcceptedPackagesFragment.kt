a2264c34332cc5eebdbec08b082569b2db1dbc72
Abdulsalam Mansour <abdulsalam.mans96@gmail.com>
2022-09-12 08:49:16 +0300

    LD-1530 - Change the naming of "accepted packages" card to "pending packages"

--- app/src/main/java/com/logestechs/driver/ui/acceptedPackages/AcceptedPackagesFragment.kt
+++ app/src/main/java/com/logestechs/driver/ui/acceptedPackages/AcceptedPackagesFragment.kt
@@ -63,7 +63,7 @@ class AcceptedPackagesFragment : LogesTechsFragment(), AcceptedPackagesCardListe
         initListeners()
         callGetAcceptedPackages()
         activityDelegate = activity as DriverPackagesByStatusViewPagerActivityDelegate
-        binding.textTitle.text = getString(R.string.packages_view_pager_pending_packages)
+        binding.textTitle.text = getString(R.string.packages_view_pager_accepted_packages)
     }
 
     override fun onResume() {
