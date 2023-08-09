4e56dc0e0ce479ee4eb70d1d18de8a149962dbd8
Ali Malluh <ali.mallouh16@gmail.com>
2023-08-09 15:53:44 +0300

    LB-5159 bug fixed

--- app/src/main/java/com/logestechs/driver/ui/pickedFulfilmentOrdersActivity/PickedFulfilmentOrdersActivity.kt
+++ app/src/main/java/com/logestechs/driver/ui/pickedFulfilmentOrdersActivity/PickedFulfilmentOrdersActivity.kt
@@ -22,6 +22,7 @@ import com.logestechs.driver.utils.FulfilmentOrderStatus
 import com.logestechs.driver.utils.Helper
 import com.logestechs.driver.utils.IntentExtrasKeys
 import com.logestechs.driver.utils.LogesTechsActivity
+import com.logestechs.driver.utils.adapters.NewFulfilmentOrderCellAdapter
 import com.logestechs.driver.utils.adapters.PickedFulfilmentOrderCellAdapter
 import com.logestechs.driver.utils.interfaces.PickedFulfilmentOrderCardListener
 import kotlinx.coroutines.Dispatchers
@@ -87,7 +88,12 @@ class PickedFulfilmentOrdersActivity : LogesTechsActivity(), PickedFulfilmentOrd
         tabLayout.getTabAt(0)?.select()
         callGetFulfilmentOrders(FulfilmentOrderStatus.PICKED.name)
     }
-
+    override fun onResume() {
+        super.onResume()
+        currentPageIndex = 1
+        (binding.rvFulfilmentOrders.adapter as PickedFulfilmentOrderCellAdapter).clearList()
+        callGetFulfilmentOrders(status)
+    }
 
     private fun initRecycler() {
         val layoutManager = LinearLayoutManager(
