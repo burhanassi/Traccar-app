7a61ab6fdc368fb5c3f023f9e6095a56e94c7660
Ali Malluh <alimalluh@logestechs.com>
2024-06-30 17:13:00 +0300

    LB-11089 Hide the social media icons from the package card in the driver application.

--- app/src/main/java/com/logestechs/driver/data/model/DriverCompanyConfigurations.kt
+++ app/src/main/java/com/logestechs/driver/data/model/DriverCompanyConfigurations.kt
@@ -37,5 +37,7 @@ data class DriverCompanyConfigurations(
     var isScanAllPackageAwbCopiesByDriver: Boolean? = null,
     var isAddingPaymentTypesEnabled: Boolean? = null,
     var isPreventDriversDeliveredPickupPackages: Boolean? = null,
-    var isForceDriversToAddAttachments: Boolean? = null
+    var isForceDriversToAddAttachments: Boolean? = null,
+    var isHideSenderInfo: Boolean? = null,
+    var isShowSenderPhone: Boolean? = null
 )
