53787adfaff08aa29489b271fbce51f92310461a
Burhan <burhan@logestechs.com>
2025-05-18 09:17:59 +0300

    static in dynamic

--- app/src/main/java/com/logestechs/driver/data/model/CodCollectionMethod.kt
+++ app/src/main/java/com/logestechs/driver/data/model/CodCollectionMethod.kt
@@ -3,5 +3,6 @@ package com.logestechs.driver.data.model
 data class CodCollectionMethod(
     val id: Int,
     val paymentType: String?,
-    val paymentTypeName: String?
+    val paymentTypeName: String?,
+    val staticPaymentTypeName: String?
 )
