6bf227f1bb844fdbabe643c763c25ca111c4c218
Burhan <burhan@logestechs.com>
2025-06-04 16:10:05 +0300

    feat(Return): Driver Android - Alyamamah Company only - remove the option for "Receiver paid costs " when returning packages.
    refers to LB-16402

--- app/src/main/java/com/logestechs/driver/utils/dialogs/ReturnPackageDialog.kt
+++ app/src/main/java/com/logestechs/driver/utils/dialogs/ReturnPackageDialog.kt
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.logestechs.driver.R
 import com.logestechs.driver.api.ApiAdapter
 import com.logestechs.driver.api.requests.ReturnPackageRequestBody
+import com.logestechs.driver.data.model.CompanyInfo
 import com.logestechs.driver.data.model.DriverCompanyConfigurations
 import com.logestechs.driver.data.model.LoadedImage
 import com.logestechs.driver.data.model.Package
@@ -41,7 +42,7 @@ class ReturnPackageDialog(
 
     lateinit var binding: DialogReturnPackageBinding
     lateinit var alertDialog: AlertDialog
-
+    private var companyInfo: CompanyInfo? = SharedPreferenceWrapper.getDriverCompanyInfo()
     private var companyConfigurations: DriverCompanyConfigurations? =
         SharedPreferenceWrapper.getDriverCompanySettings()?.driverCompanyConfigurations
 
@@ -56,6 +57,11 @@ class ReturnPackageDialog(
         val alertDialog = dialogBuilder.create()
         this.binding = binding
 
+        if (companyInfo?.id == 397.toLong()) {
+            binding.switchReceiverPaidCostsContainer.visibility = View.GONE
+        } else {
+            binding.switchReceiverPaidCostsContainer.visibility = View.VISIBLE
+        }
         if (pkg?.isReceiverPayCost == true) {
             binding.switchReceiverPaidCosts.isChecked = true
             if (pkg?.partnerPackageId != null) {
